581176e2fc914c316ce69ad91197de7e7aa0b9db
vkhomyn95 <vkhomyn95@gmail.com>
2018-09-21 22:33:00 +0300

    validation fix

--- storages/forms.py
+++ storages/forms.py
@@ -10,25 +10,14 @@ class StorageAddForm(forms.ModelForm):
         fields = ('name', 'city', 'address')
 
     def clean_name(self):
-        name = self.cleaned_data['name']
-        try:
-            match = Storage.objects.get(name=name)
-        except:
-            return self.cleaned_data['name']
-        raise forms.ValidationError('Username already exist')
+        user = self.cleaned_data['name']
+        if Storage.objects.filter(name=user).exists():
+            raise forms.ValidationError('The name %s already exists' % user)
+        return user
 
-        # if Storage.objects.filter(name=user).exists():
-        #     raise forms.ValidationError('The name %s already exists' % user)
-        # return user
-
-    def val_address(self):
-        name = self.cleaned_data['name']
+    def clean_address(self):
+        name = self.cleaned_data['address']
         MAX_LENGTH = 5
         if len(name) > MAX_LENGTH:
-            raise forms.ValidationError('Password %s should not have more %s ' % MAX_LENGTH)
-    # def clean_content(self):
-    #     min_len = 5
-    #     content = self.cleaned_data.get('address')
-    #     if len(content) > min_len:
-    #         raise forms.ValidationError('content should {0}k'.format(min_len))
-    #     return content
+            raise forms.ValidationError('Name cointainc %d more 5 letters' %MAX_LENGTH)
+        return name
